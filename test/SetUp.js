a81bb4597b03f76285757ff1de1e0248e9e13de7
Matt Smith <mattonfoot@gmail.com>
2013-08-12 22:28:40 +0100

    Wrote first spec along with interface design, interface not actually defined though

--- test/SetUp.js
+++ test/SetUp.js
@@ -1,9 +1,23 @@
+var chai = require("chai");
+
+chai.should();
+
+// scenarios
+
 describe('Given a Game has a group of players', function() {
+
+  var player1 = new Player('player one');
+  Var player2 = new Player('player two');
+
+  var game = Game.withPlayer(player1).withPlayer(player2).create();
 
   describe('When the game is about to begin', function() {
 
+    game.begin();
+
     it('Then a random player will be assigned as the dealer', function() {
 
+      [player1, player2].should.contain(game.dealer.player);
     });
   });
 });
